8b1efcc477da52007ebeae1d1cd42a26f2c367e4
rey_imaginario <charly021@gmail.com>
2022-07-19 22:11:11 -0300

    Otros archivos

---
+++ src/test/java/com/dadasoft/gestorDeGastos/GestorDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadasoft.gestorDeGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//  @SpringBootTest
+class GestorDeGastosApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
